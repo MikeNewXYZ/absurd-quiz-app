f8cd523e5bce850822f6bcda679ac3328679fecc
Mike Newberry <hello@mikenew.xyz>
2024-05-21 17:36:26 +0100

    add loading sequence and change slide function

--- client/src/main.js
+++ client/src/main.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import "@phosphor-icons/web/bold";
 import randomTheme from "./lib/random-theme";
 import fadeIn from "./lib/fade-in";
 import JSConfetti from "js-confetti";
@@ -6,7 +7,7 @@ import JSConfetti from "js-confetti";
 const bodyEl = document.querySelector("body");
 const jsConfetti = new JSConfetti();
 
-// Page intro sequence
+// Page intro sequence.
 async function init() {
 	await randomTheme(bodyEl);
 	await fadeIn(bodyEl);
@@ -14,7 +15,28 @@ async function init() {
 }
 init();
 
-// When any button element is clicked run js confetti
+// When any button element is clicked run js confetti.
 [...document.querySelectorAll("button")].forEach((el) => {
 	el.addEventListener("click", () => jsConfetti.addConfetti());
+});
+
+// When button with "data-next-slide" attribute is clicked
+// get its slide value, then hide all slides that don't
+// match and show the one that does. Also change to
+// a random theme.
+[...document.querySelectorAll("[data-next-slide]")].forEach((el) => {
+	el.addEventListener("click", () => {
+		const nextSlideId = el.getAttribute("data-next-slide");
+
+		[...document.querySelectorAll("[data-slide]")].forEach((el) => {
+			const slideId = el.getAttribute("data-slide");
+
+			if (slideId === nextSlideId) {
+				el.style.display = "block";
+				randomTheme(bodyEl);
+			} else {
+				el.style.display = "none";
+			}
+		});
+	});
 });
